755efe387bf0ca5b59dca01944aa0dc119ff0e46
David Megginson <david.megginson@gmail.com>
2024-10-06 13:56:37 -0400

    Made texture a bit more interesting.

---
+++ Makefile
@@ -0,0 +1,4 @@
+SCENERY=${HOME}/.local/share/scenery/local-scenery-mods
+
+all:
+	cp coastguard-icebreaker.ac coastguard-icebreaker.png ${SCENERY}/Models/Maritime/Military
---
+++ start-heli.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Start an aircraft on the heli deck of the vessel in its initial/default location
+
+fgfs --lon=-75.51573778 --lat=44.70663363 --heading=225 --altitude=300 "$*"
